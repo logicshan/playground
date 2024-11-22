ab4d7fcbd2876c272528f2de1babfbe939493214
格罗滕迪克 <10594774+grotengdick@user.noreply.gitee.com>
2024-11-22 13:24:38 +0800

    https://proofassistants.stackexchange.com/questions/900/when-should-i-use-de-bruijn-levels-instead-of-indices

---
+++ kovacs/README.md
@@ -0,0 +1,1 @@
+https://proofassistants.stackexchange.com/questions/900/when-should-i-use-de-bruijn-levels-instead-of-indices
---
+++ kovacs/t.hs
@@ -0,0 +1,27 @@
+type Ix  = Int
+type Lvl = Int
+
+data Tm      = Var Ix
+             | Lam Tm
+             | App Tm Tm
+data Closure = Close [Val] Tm
+data Val     = VVar Lvl
+             | VApp Val Val
+             | VLam Closure
+
+eval :: [Val] -> Tm -> Val
+eval env t = case t of
+  Var x   -> env !! x
+  App t u -> case (eval env t, eval env u) of
+               (VLam (Close env' t), u) -> eval (u:env') t
+               (t                  , u) -> VApp t u
+  Lam t   -> VLam (Close env t)
+
+quote :: Lvl -> Val -> Tm
+quote l t = case t of
+  VVar x             -> Var (l - x - 1)
+  VApp t u           -> App (quote l t) (quote l u)
+  VLam (Close env t) -> Lam (quote (l + 1) (eval (VVar l:env) t))
+
+nf :: [Val] -> Tm -> Tm
+nf env t = quote (length env) (eval env t)
